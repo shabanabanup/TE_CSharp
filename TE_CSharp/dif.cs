ac185a621acd7879c8fbba049b51f15cc0fb1c03
shabanabanup <shabana.banu7444@gmail.com>
2022-10-04 16:48:34 +0530

    commit

---
+++ TE_CSharp/dif.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp
+{
+        public class Internal
+        {
+            public string ecourse;
+
+            public void std_details(string course)
+            {
+                ecourse = course;
+            Console.WriteLine("---------------");
+            Console.WriteLine($"{ecourse}");
+            Console.WriteLine("---------------");
+            }
+        }
+        public class Internal_public
+        {
+            public static void Main()
+            {
+                Internal inter = new Internal();
+                inter.std_details("DOTNET SREAM");
+            }
+        }
+    
+}
---
+++ TE_CSharp/inher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp
+{
+    public class Bonus
+    {
+        protected int bonus;
+    }
+    public class Staff : Bonus
+    {
+        protected int salary = 30980;
+        public void GetBonus1()
+        {
+            bonus = 3 * salary;
+            Console.WriteLine($"Staff salary: {salary} and Bonus= {bonus}");
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+    public class NonStaff : Staff
+    {
+        protected int salary = 15980;
+        public void GetBonus2()
+        {
+            bonus = 2 * salary;
+            Console.WriteLine($"Non Staff Salary: {salary} and Bonus= {bonus}");
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+    internal class inher
+    {
+        class Program
+        {
+            public static void Main()
+            {
+                Staff s = new Staff();
+                s.GetBonus1();
+                NonStaff n = new NonStaff();
+                n.GetBonus2();
+            }
+        }
+    }
+}
