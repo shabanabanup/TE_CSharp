56cff88420e3fb151ac95de854b2387ddf26c5f9
shabanabanup <shabana.banu7444@gmail.com>
2022-10-06 08:50:16 +0530

    commit

---
+++ TE_CSharp/Operate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp
+{
+    internal class Operate
+    { 
+            public delegate T AreaDelegate<T>(T r);
+            class area
+            {
+                public double area1(double r)
+                {
+                    return 3.142 * r * r;
+                }
+                public double area2(double a)
+                {
+                    return a * a;
+                }
+                public int area2(int a)
+                {
+                    return a * a;
+                }
+                public double area3(double b)
+                {
+                    return 6 * b * b;
+                }
+                public int area3(int b)
+                {
+                    return 6 * b * b;
+                }
+            }
+            class calculate
+            {
+                static void Main(string[] args)
+                {
+                    Console.WriteLine("Areas of :Circle,Square(d)(i),Cube");
+                Console.WriteLine("-------------------------------------");
+                area c = new area();
+                    //double
+                    AreaDelegate<double> dlgt = new AreaDelegate<double>(c.area1);
+                    Console.WriteLine("Circle = " + dlgt(5));
+                Console.WriteLine("-------------------------------------");
+
+                    dlgt = c.area2;
+                    Console.WriteLine("Square(double) = " + dlgt(4));
+                Console.WriteLine("-------------------------------------");
+
+                dlgt = c.area3;
+                    Console.WriteLine("Cube(double) = " + dlgt(6));
+                Console.WriteLine("-------------------------------------");
+                //int
+                AreaDelegate<int> dlgt1 = new AreaDelegate<int>(c.area2);
+                    Console.WriteLine("Square (int) = " + dlgt1(8));
+                Console.WriteLine("-------------------------------------");
+
+                dlgt = c.area3;
+                    Console.WriteLine("Square(int) = " + dlgt1(4));
+                Console.WriteLine("-------------------------------------");
+            }
+            }
+        
+    }
+}
+
---
+++ TE_CSharp/coll.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TE_CSharp
+{
+    internal class coll
+    {
+        
+            static public void Main()
+            {
+                //dictionary
+                Dictionary<int, string> d = new Dictionary<int, string>();
+
+                d.Add(107, "Willson");
+                d.Add(209, "Thomas");
+                d.Add(305, "Mark");
+
+                //hastable
+                Hashtable h = new Hashtable();
+
+                h.Add(107, ".NET");
+                h.Add(209, ".NET");
+                h.Add(305, "Database");
+
+                foreach (KeyValuePair<int, string> element in d)
+                {
+                    Console.WriteLine("ID: {0} and Name: {1}", element.Key, element.Value);
+                Console.WriteLine("-------------------------------------");
+            }
+                Console.WriteLine("\n");
+                Console.WriteLine("-------------------------------------");
+                foreach (DictionaryEntry element in h)
+                {
+                    Console.WriteLine("ID: {0} and Batch: {1}", element.Key, element.Value);
+                Console.WriteLine("-------------------------------------");
+            }
+            }
+        
+    }
+}
+
